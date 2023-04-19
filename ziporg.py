5919a644db29f6506f255fc334d9c79d8ad2af12
korpugglan <korpugglan@hotmail.com>
2023-04-19 12:40:51 +0200

    Add menu selection

--- ziporg.py
+++ ziporg.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # Script to batch process and organize zip files
+# TODO: Add webp to jpg feature
 
 # Import packages
 import sys
@@ -18,11 +19,54 @@ def print_line(print_chars="-", repetition=100):
     return
 
 
-# Define global variables
+def select_from_menu(menu_dict, selection_text):
+    """Select a value from a dictionary through an input menu.
+            Args:
+                menu_dict (dict): A dictionary containing menu items
+                selection_text (str): A string describing the menu items
+            Returns:
+                selection (str): A key value for the input dictionary
+    """
+    print_line()
+    for item in menu_dict:
+        print(f"({item}) {menu_dict[item]}")
+
+    while True:
+        print_line()
+        selection = input(f"Please select {selection_text} by typing the value in brackets: ")
+        if selection == "exit":
+            quit_script()
+        elif selection not in menu_dict:
+            print_line()
+            print("Incorrect input. Please select a value from the list or type \"exit\"")
+        else:
+            return selection
 
 
-if __name__ == "__main__":
+def quit_script():
+    """Prints a message and quits the script.
+        Args: None
+        Returns: None
+    """
     print_line("=")
     print("Ciao bella, ciao")
     print_line("=")
     sys.exit()
+
+
+# Define global variables
+feat_dict = {"1": "Rename zip files",
+             "2": "Unzip and rename into single folder",
+             "3": "Unzip and unpack into separate folders"}
+
+if __name__ == "__main__":
+    # Welcome text
+    print_line("=")
+    print("Welcome to ZIPORG")
+    print("Entering the phrase \"exit\" always quits the script")
+
+    # Feature select
+    feat_select = select_from_menu(feat_dict, "feature")
+    print(f"{feat_select} selected. This means \"{feat_dict[feat_select]}\"")
+
+    # quit_script()
